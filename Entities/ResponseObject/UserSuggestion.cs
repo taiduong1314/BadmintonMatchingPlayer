4fb79e3dad39883f0a93103de17d27a62860a7bb
taiduong1314 <taiduong1314@gmail.com>
2023-11-24 13:37:57 +0700

    AAAAA

--- Entities/ResponseObject/UserSuggestion.cs
+++ Entities/ResponseObject/UserSuggestion.cs
@@ -6,5 +6,6 @@
         public string? Name { get; set; }
         public string? ShortProfile { get; set; }
         public double? Rating { get; set; }
+        public bool? IsSub {  get; set; }
     }
 }
