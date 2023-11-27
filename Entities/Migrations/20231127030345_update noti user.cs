c585ccabf36dad6969b969facb9a2de03632312a
taiduong1314 <taiduong1314@gmail.com>
2023-11-27 10:34:01 +0700

    [main]edit db

---
+++ Entities/Migrations/20231127030345_update noti user.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entities.Migrations
+{
+    public partial class updatenotiuser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAndroidDevice",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LogingingDevice",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Notifications",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAndroidDevice",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LogingingDevice",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Notifications");
+        }
+    }
+}
