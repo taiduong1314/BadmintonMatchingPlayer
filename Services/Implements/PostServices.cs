0de5e5048252803979cd1d517ef30fb8af4f99c5
taiduong1314 <taiduong1314@gmail.com>
2023-12-18 22:51:29 +0700

    [main]temp push

--- Services/Implements/PostServices.cs
+++ Services/Implements/PostServices.cs
@@ -228,16 +228,16 @@ namespace Services.Implements
                 res[i] = cPost;
             }
 
-           var returnList =new List<PostOptional>();
-            foreach(var postOptional in res)
-            {
-                if(postOptional.QuantitySlot!=null && postOptional.QuantitySlot != 0) 
-                {
-                    returnList.Add(postOptional);
-                }
-            }
+           //var returnList =new List<PostOptional>();
+           // foreach(var postOptional in res)
+           // {
+           //     if(postOptional.QuantitySlot!=null && postOptional.QuantitySlot != 0) 
+           //     {
+           //         returnList.Add(postOptional);
+           //     }
+           // }
 
-            return returnList;
+            return res;
         }
 
         private bool IsPostValid(Post post)
